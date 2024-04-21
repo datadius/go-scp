2fbfd1cc6b9b0227ca89221e788b581db855fe9d
Data Dius <16731794+datadius@users.noreply.github.com>
2024-04-21 22:49:54 +0200

    working TUI

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/bramvdbogaerde/go-scp/auth"
+	"golang.org/x/crypto/ssh"
+	"main/scp"
+)
+
+func main() {
+	clientConfig, err := auth.PasswordKey("claud", "Hq$8ndX5p6$c", ssh.InsecureIgnoreHostKey())
+	if err != nil {
+		fmt.Print("Failed to create config: ", err)
+		return
+	}
+
+	client := scp.NewClient("localhost:22", &clientConfig)
+
+	err = client.Connect()
+	if err != nil {
+		fmt.Print("Failed to connect: ", err)
+		return
+	}
+
+	f, err := os.Create("test.mp4")
+	if err != nil {
+		fmt.Print("Failed to open: ", err)
+		return
+	}
+
+	defer client.Close()
+
+	defer f.Close()
+
+	// if the connection requires a PTY, then it will not work
+	//err = client.CopyFromRemoteProgressPassThru(context.Background(), f, "hello.txt", nil)
+
+	err = client.CopyFromRemoteProgressPassThru(context.Background(), f, "test.mp4", nil)
+
+	if err != nil {
+		fmt.Print("Failed to copy: ", err)
+		return
+	}
+}
--- scp/client.go
+++ scp/client.go
@@ -12,12 +12,13 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path"
 	"sync"
 	"time"
 
+	"github.com/charmbracelet/bubbles/progress"
+	tea "github.com/charmbracelet/bubbletea"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -85,13 +86,24 @@ func (a *Client) SSHClient() *ssh.Client {
 }
 
 // CopyFromFile copies the contents of an os.File to a remote location, it will get the length of the file by looking it up from the filesystem.
-func (a *Client) CopyFromFile(ctx context.Context, file os.File, remotePath string, permissions string) error {
+func (a *Client) CopyFromFile(
+	ctx context.Context,
+	file os.File,
+	remotePath string,
+	permissions string,
+) error {
 	return a.CopyFromFilePassThru(ctx, file, remotePath, permissions, nil)
 }
 
 // CopyFromFilePassThru copies the contents of an os.File to a remote location, it will get the length of the file by looking it up from the filesystem.
 // Access copied bytes by providing a PassThru reader factory.
-func (a *Client) CopyFromFilePassThru(ctx context.Context, file os.File, remotePath string, permissions string, passThru PassThru) error {
+func (a *Client) CopyFromFilePassThru(
+	ctx context.Context,
+	file os.File,
+	remotePath string,
+	permissions string,
+	passThru PassThru,
+) error {
 	stat, err := file.Stat()
 	if err != nil {
 		return fmt.Errorf("failed to stat file: %w", err)
@@ -101,21 +113,39 @@ func (a *Client) CopyFromFilePassThru(ctx context.Context, file os.File, remoteP
 
 // CopyFile copies the contents of an io.Reader to a remote location, the length is determined by reading the io.Reader until EOF
 // if the file length in know in advance please use "Copy" instead.
-func (a *Client) CopyFile(ctx context.Context, fileReader io.Reader, remotePath string, permissions string) error {
+func (a *Client) CopyFile(
+	ctx context.Context,
+	fileReader io.Reader,
+	remotePath string,
+	permissions string,
+) error {
 	return a.CopyFilePassThru(ctx, fileReader, remotePath, permissions, nil)
 }
 
 // CopyFilePassThru copies the contents of an io.Reader to a remote location, the length is determined by reading the io.Reader until EOF
 // if the file length in know in advance please use "Copy" instead.
 // Access copied bytes by providing a PassThru reader factory.
-func (a *Client) CopyFilePassThru(ctx context.Context, fileReader io.Reader, remotePath string, permissions string, passThru PassThru) error {
-	contentsBytes, err := ioutil.ReadAll(fileReader)
+func (a *Client) CopyFilePassThru(
+	ctx context.Context,
+	fileReader io.Reader,
+	remotePath string,
+	permissions string,
+	passThru PassThru,
+) error {
+	contentsBytes, err := io.ReadAll(fileReader)
 	if err != nil {
 		return fmt.Errorf("failed to read all data from reader: %w", err)
 	}
 	bytesReader := bytes.NewReader(contentsBytes)
 
-	return a.CopyPassThru(ctx, bytesReader, remotePath, permissions, int64(len(contentsBytes)), passThru)
+	return a.CopyPassThru(
+		ctx,
+		bytesReader,
+		remotePath,
+		permissions,
+		int64(len(contentsBytes)),
+		passThru,
+	)
 }
 
 // wait waits for the waitgroup for the specified max timeout.
@@ -153,13 +183,26 @@ func checkResponse(r io.Reader) error {
 }
 
 // Copy copies the contents of an io.Reader to a remote location.
-func (a *Client) Copy(ctx context.Context, r io.Reader, remotePath string, permissions string, size int64) error {
+func (a *Client) Copy(
+	ctx context.Context,
+	r io.Reader,
+	remotePath string,
+	permissions string,
+	size int64,
+) error {
 	return a.CopyPassThru(ctx, r, remotePath, permissions, size, nil)
 }
 
 // CopyPassThru copies the contents of an io.Reader to a remote location.
 // Access copied bytes by providing a PassThru reader factory
-func (a *Client) CopyPassThru(ctx context.Context, r io.Reader, remotePath string, permissions string, size int64, passThru PassThru) error {
+func (a *Client) CopyPassThru(
+	ctx context.Context,
+	r io.Reader,
+	remotePath string,
+	permissions string,
+	size int64,
+	passThru PassThru,
+) error {
 	session, err := a.sshClient.NewSession()
 	if err != nil {
 		return fmt.Errorf("Error creating ssh session in copy to remote: %v", err)
@@ -272,7 +315,12 @@ func (a *Client) CopyFromRemote(ctx context.Context, file *os.File, remotePath s
 // CopyFromRemotePassThru copies a file from the remote to the given writer. The passThru parameter can be used
 // to keep track of progress and how many bytes that were download from the remote.
 // `passThru` can be set to nil to disable this behaviour.
-func (a *Client) CopyFromRemotePassThru(ctx context.Context, w io.Writer, remotePath string, passThru PassThru) error {
+func (a *Client) CopyFromRemotePassThru(
+	ctx context.Context,
+	w io.Writer,
+	remotePath string,
+	passThru PassThru,
+) error {
 	session, err := a.sshClient.NewSession()
 	if err != nil {
 		return fmt.Errorf("Error creating ssh session in copy from remote: %v", err)
@@ -362,6 +410,182 @@ func (a *Client) CopyFromRemotePassThru(ctx context.Context, w io.Writer, remote
 			errCh <- err
 			return
 		}
+	}()
+
+	if a.Timeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, a.Timeout)
+		defer cancel()
+	}
+
+	if err := wait(&wg, ctx); err != nil {
+		return err
+	}
+	finalErr := <-errCh
+	close(errCh)
+	return finalErr
+}
+
+var p *tea.Program
+
+type progressWriter struct {
+	total      int64
+	downloaded int
+	file       io.Writer
+	reader     io.Reader
+	onProgress func(float64)
+}
+
+func (pw *progressWriter) Start() error {
+	_, err := CopyN(pw.file, io.TeeReader(pw.reader, pw), pw.total)
+	//var total int64
+	//total = 0
+	//for total < pw.total {
+	//	n, err := CopyN(pw.file, pw.reader, pw.total)
+	//	pw.downloaded += n
+	//	if pw.total > 0 && pw.onProgress != nil {
+	//		pw.onProgress(float64(pw.downloaded) / float64(pw.total))
+	//	}
+	//	if err != nil {
+	//		fmt.Println(err)
+	//		p.Send(progressErrMsg{err})
+	//	}
+	//	total += n
+	//}
+
+	//_, err := CopyN(pw.file, pw.reader, pw.total)
+	if err != nil {
+		p.Send(progressErrMsg{err})
+	}
+	return err
+}
+
+func (pw *progressWriter) Write(p []byte) (int, error) {
+	pw.downloaded += len(p)
+	if pw.total > 0 && pw.onProgress != nil {
+		pw.onProgress(float64(pw.downloaded) / float64(pw.total))
+	}
+	return len(p), nil
+}
+
+func (a *Client) CopyFromRemoteProgressPassThru(
+	ctx context.Context,
+	w io.Writer,
+	remotePath string,
+	passThru PassThru,
+) error {
+	session, err := a.sshClient.NewSession()
+	if err != nil {
+		return fmt.Errorf("Error creating ssh session in copy from remote: %v", err)
+	}
+	defer session.Close()
+
+	wg := sync.WaitGroup{}
+	errCh := make(chan error, 4)
+
+	wg.Add(1)
+	go func() {
+		var err error
+
+		defer func() {
+			// NOTE: this might send an already sent error another time, but since we only receive opne, this is fine. On the "happy-path" of this function, the error will be `nil` therefore completing the "err<-errCh" at the bottom of the function.
+			errCh <- err
+			// We must unblock the go routine first as we block on reading the channel later
+			wg.Done()
+
+		}()
+
+		r, err := session.StdoutPipe()
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		in, err := session.StdinPipe()
+		if err != nil {
+			errCh <- err
+			return
+		}
+		defer in.Close()
+
+		err = session.Start(fmt.Sprintf("%s -f %q", a.RemoteBinary, remotePath))
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		err = Ack(in)
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		res, err := ParseResponse(r)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		if res.IsFailure() {
+			errCh <- errors.New(res.GetMessage())
+			return
+		}
+
+		infos, err := res.ParseFileInfos()
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		err = Ack(in)
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		if passThru != nil {
+			r = passThru(r, infos.Size)
+		}
+
+		pw := &progressWriter{
+			total:  infos.Size,
+			file:   w,
+			reader: r,
+			onProgress: func(ratio float64) {
+				p.Send(progressMsg(ratio))
+			},
+		}
+
+		m := model{
+			pw:       pw,
+			progress: progress.New(progress.WithDefaultGradient()),
+		}
+
+		p = tea.NewProgram(m)
+
+		go pw.Start()
+
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		err = Ack(in)
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		if _, err := p.Run(); err != nil {
+			fmt.Println("Error running progress: ", err)
+			os.Exit(1)
+		}
+
+		err = session.Wait()
+		if err != nil {
+			errCh <- err
+			return
+		}
+
 	}()
 
 	if a.Timeout > 0 {
